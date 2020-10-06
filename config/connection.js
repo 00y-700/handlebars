cbdd1909fb790c55cc240f60c1f34502796c121e
Jeffrey Harding <jeffreyh202@gmail.com>
2020-10-06 14:43:28 -0600

    testing

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var pass = require("../security/password.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: pass,
+    database: "burgers_db"
+  });
+}
 
 
 connection.connect(function(err) {
