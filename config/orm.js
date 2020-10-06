233b8ef753096b8f19d84b4e0edda178d3c69dd7
Jeffrey Harding <jeffreyh202@gmail.com>
2020-10-06 16:25:15 -0600

    styling

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,6 @@
-
-// Here is the O.R.M. where you write functions that takes inputs and conditions
-// and turns them into database commands like SQL.
-
 var connection = require("./connection.js");
 
-function printQuestionMarks(num) {
-  var arr = [];
-
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
 function objToSql(ob) {
-  // column1=value, column2=value2,...
   var arr = [];
 
   for (var key in ob) {
@@ -35,17 +20,9 @@ var orm = {
       cb(result);
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
+  
   create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
-
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+    var queryString = `INSERT INTO ${table} (${cols}) VALUES (?, ?);`
 
     console.log(queryString);
 
@@ -56,15 +33,10 @@ var orm = {
       cb(result);
     });
   },
-  // objColVals would be the columns and values that you want to update
-  // an example of objColVals would be {name: panther, sleepy: true}
+  
   update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+    var cVal = objToSql(objColVals)
+    var queryString = `UPDATE ${table} SET ${cVal} WHERE ${condition};`
 
     console.log(queryString);
     connection.query(queryString, function(err, result) {
